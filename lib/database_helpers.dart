f1836447fd4ff3e829df9962389ef04e3b224fec
Leung Wing Man <vaniceleung@Leungs-MacBook-Air.local>
2020-03-02 20:34:49 +0800

    Put the app bar + search bar in another file

---
+++ lib/database_helpers.dart
@@ -0,0 +1,8 @@
+class Contact {
+  int id;
+  String name;
+  String phoneNum;
+  String photo;
+
+  Contact({this.id, this.name, this.phoneNum, this.photo});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/myAppBar.dart';
+
 void main() {
   runApp(MaterialApp(
     home: MyApp(),
@@ -18,62 +20,7 @@ class _MyAppState extends State<MyApp> {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          Stack(
-            children: <Widget>[
-              Container(
-                height: mediaquery.size.height * 0.2,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [const Color(0xff4069B9), const Color(0xFF284171)],
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    SizedBox(width: mediaquery.size.width * 0.1),
-                    Text(
-                      "CONTACT",
-                      style: TextStyle(color: Colors.white, fontSize: 18),
-                    ),
-                    IconButton(
-                      icon: Icon(
-                        Icons.add,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {},
-                    )
-                  ],
-                ),
-              ),
-              Center(
-                child: Container(
-                  width: mediaquery.size.width * 0.8,
-                  height: mediaquery.size.height * 0.05,
-                  margin: EdgeInsets.only(top: mediaquery.size.height * 0.18),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.all(Radius.circular(30)),
-                    color: Colors.white,
-                  ),
-                  child: Container(
-                    padding: EdgeInsets.only(left: 10, bottom: 4),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Search Contact",
-                        suffixIcon: IconButton(
-                            icon: Icon(Icons.search),
-                            onPressed: () {
-                              print("Search");
-                              FocusScope.of(context).requestFocus(FocusNode());
-                            }),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
+          MyAppBar(),
         ],
       ),
     );
